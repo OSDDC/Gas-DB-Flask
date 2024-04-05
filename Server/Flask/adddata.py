06ede0cb191d3680123181f1a361788addf2d559
Marco <it.marco2807@gmail.com>
2024-04-05 16:29:32 +0200

    adddata.py

--- Server/Flask/adddata.py
+++ Server/Flask/adddata.py
@@ -1,8 +1,27 @@
-data = input(Please input collected Data here:)
+import json
 
-# Dummy-Daten in JSON-Datei speichern
-json_file_path = 'data.json'
-with open(json_file_path, 'a') as file:
-    json.dump(dummy_data, file, indent=2)
+def main():
+    # Lese die vorhandene data.json-Datei ein
+    try:
+        with open('data.json', 'r') as json_file:
+            existing_data = json.load(json_file)
+    except FileNotFoundError:
+        print("Die Datei 'data.json' wurde nicht gefunden. Bitte erstelle sie zuerst.")
+        return
 
-print(f'Daten wurden erfolgreich in "{json_file_path}" hinzugefügt.')
+    # Benutzereingabe für neue Daten
+    data = input("Add new data: ")
+
+
+    # Füge die neuen Daten zur vorhandenen Liste hinzu
+    new_entry = {data}
+    existing_data.append(new_entry)
+
+    # Schreibe die aktualisierten Daten zurück in die data.json-Datei
+    with open('data.json', 'w') as json_file:
+        json.dump(existing_data, json_file, indent=4)
+
+    print(f"Die Daten wurden erfolgreich zur 'data.json' hinzugefügt:\n{new_entry}")
+
+if __name__ == "__main__":
+    main()
